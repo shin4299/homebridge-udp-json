f8ee7bbd084277d8cf5ab53606b64445e2e07996
shin4299 <shin4299@gmail.com>
2018-05-02 23:28:27 +0900

    Update udp-json.js

--- udp-json.js
+++ udp-json.js
@@ -78,7 +78,7 @@ class UDPJSONPlugin
    // if (co2_ppm !== false) {
 	this.carbondioxideService
 	.getCharacteristic(Characteristic.CarbonDioxideDetected)
-	.setValue(co2_ppm > 1200 ? Characteristic.CarbonDioxideDetected.CO2_LEVELS_ABNORMAL : Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL)
+	.setValue(co2_ppm > 800 ? Characteristic.CarbonDioxideDetected.CO2_LEVELS_ABNORMAL : Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL)
 	.getCharacteristic(Characteristic.CarbonDioxideLevel)
 	.setValue(Math.round(co2_ppm))	  
     //}
