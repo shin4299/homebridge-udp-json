76933022891f1cc7736f88bfb15d4e385d7c795b
shin4299 <shin4299@gmail.com>
2018-05-03 02:48:58 +0900

    Update udp-json.js

--- udp-json.js
+++ udp-json.js
@@ -41,7 +41,7 @@ class UDPJSONPlugin
         maxValue: 100
       });
 
-    	this.humidityService = new Service.HumiditySensor(this.name_humidity);	    
+    	this.humidityService = if (this.humidityOff != "") { new Service.HumiditySensor(this.name_humidity)};	    
 	  
 	 this.carbondioxideService = new Service.CarbonDioxideSensor(this.name_carbonDioxide);
 	  
@@ -105,7 +105,7 @@ class UDPJSONPlugin
 
   getServices() {
 	  
-return [this.informationService],
+/*return [this.informationService],
 	  
 if (this.temperatureOff != "") { 
 	return [this.temperatureService]
@@ -118,8 +118,8 @@ if (this.carbonDioxideOff != "") {
 }
 if (this.lightOff != "") { 
 	return [this.lightService]
-}
-//    return [this.informationService, this.temperatureService, this.carbondioxideService, this.lightService]
+}*/
+    return [this.informationService, this.humidityService, this.temperatureService, this.carbondioxideService, this.lightService]
   }
 }
-//this.humidityService, 
+//
