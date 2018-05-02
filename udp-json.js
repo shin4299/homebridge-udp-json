7ff4b154abdc6f41dbe8572102a2205aa4bafaba
shin4299 <shin4299@gmail.com>
2018-05-03 01:52:38 +0900

    Update udp-json.js

--- udp-json.js
+++ udp-json.js
@@ -26,36 +26,6 @@ class UDPJSONPlugin
     this.name_light = config.name_light || this.name;
     this.listen_port = config.listen_port || 8268;
 	  
- getServices() {
-	 var services = [],
-	     
-    this.informationService = new Service.AccessoryInformation();
-
-    this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, "Bosch")
-      .setCharacteristic(Characteristic.Model, "RPI-UDPJSON")
-      .setCharacteristic(Characteristic.SerialNumber, this.device);
-
-if (this.temperatureOff != "") { 
-   this.temperatureService = new Service.TemperatureSensor(this.name_temperature);	    
-    this.temperatureService
-      .getCharacteristic(Characteristic.CurrentTemperature)
-      .setProps({
-        minValue: -100,
-        maxValue: 100
-      });
- }
-	  
-if (this.humidityOff != "") { 
-    	this.humidityService = new Service.HumiditySensor(this.name_humidity);	    
-}
-if (this.carbonDioxideOff != "") { 
-	 this.carbondioxideService = new Service.CarbonDioxideSensor(this.name_carbonDioxide);
-}
-if (this.lightOff != "") { 
-	this.lightService = new Service.LightSensor(this.name_light);
-}
-
     this.server = dgram.createSocket('udp4');
     
     this.server.on('error', (err) => {
@@ -81,38 +51,61 @@ if (this.lightOff != "") {
       const co2_ppm = json.co2_ppm;
       const light_lux = json.light_lux;
 
-    if (temperature_c > -100) { 
-   	this.temperatureService
-        .getCharacteristic(Characteristic.CurrentTemperature)
-        .setValue(Math.round(temperature_c));
-    }	    
-    if (humidity_percent > 0) {
-      	this.humidityService
-        .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-        .setValue(Math.round(humidity_percent));
-    }
-    if (co2_ppm > 100) {
-	this.carbondioxideService
-	.getCharacteristic(Characteristic.CarbonDioxideDetected)
-	.setValue(co2_ppm > 800 ? Characteristic.CarbonDioxideDetected.CO2_LEVELS_ABNORMAL : Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL)
-	this.carbondioxideService
-	.getCharacteristic(Characteristic.CarbonDioxideLevel)
-	.setValue(Math.round(co2_ppm))	  
-    }
-    if (light_lux >= 0) {
-        this.lightService
-	.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
-	.setValue(Math.round(light_lux))
-    }
     });
 
     
     this.server.bind(this.listen_port);
 
-	 return services;
   }
 
-//  getServices() {
-//    return [this.informationService, this.temperatureService, this.humidityService, this.carbondioxideService, this.lightService]
+  getServices() {
+	          var services = [],
+
+	  
+	      this.informationService = new Service.AccessoryInformation();
+
+    this.informationService
+      .setCharacteristic(Characteristic.Manufacturer, "Bosch")
+      .setCharacteristic(Characteristic.Model, "RPI-UDPJSON")
+      .setCharacteristic(Characteristic.SerialNumber, this.device);
+
+if (this.temperatureOff != "" && temperature_c > -100) { 
+   this.temperatureService = new Service.TemperatureSensor(this.name_temperature);	    
+    this.temperatureService
+      .getCharacteristic(Characteristic.CurrentTemperature)
+      .setProps({
+        minValue: -100,
+        maxValue: 100
+      });
+   	this.temperatureService
+        .getCharacteristic(Characteristic.CurrentTemperature)
+        .setValue(Math.round(temperature_c));  
+ }
+	  
+if (this.humidityOff != "" && humidity_percent > 0) { 
+    	this.humidityService = new Service.HumiditySensor(this.name_humidity);	    
+      	this.humidityService
+        .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+        .setValue(Math.round(humidity_percent));
+    }
+	  
+if (this.carbonDioxideOff != "" && co2_ppm > 100) { 
+	 this.carbondioxideService = new Service.CarbonDioxideSensor(this.name_carbonDioxide);
+	this.carbondioxideService
+	.getCharacteristic(Characteristic.CarbonDioxideDetected)
+	.setValue(co2_ppm > 1200 ? Characteristic.CarbonDioxideDetected.CO2_LEVELS_ABNORMAL : Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL)
+	this.carbondioxideService
+	.getCharacteristic(Characteristic.CarbonDioxideLevel)
+	.setValue(Math.round(co2_ppm))	  
+    }
+	  
+if (this.lightOff != "" && light_lux >= 0) { 
+	this.lightService = new Service.LightSensor(this.name_light);
+        this.lightService
+	.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+	.setValue(Math.round(light_lux))
+}
+
+        return services;
   }
 }
